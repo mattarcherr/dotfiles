bc81bd6734ce63f408a3f239dd7e61bf1bb77726
MA - Arch <matthewarcherr@gmail.com>
2023-05-16 21:55:02 +0100

    added suckless programs

---
+++ SL/dwmblock/blocks.def.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/    /*Command*/	                    /*Update Interval*/	/*Update Signal*/
+        {"",        "~/scripts/dwmblocks/sb-volume",         5,                  1}, 
+	{"",        "~/scripts/dwmblocks/sb-battery",        5,		         2}, 
+	{"",        "~/scripts/dwmblocks/sb-backlight",      5,		         3}, 
+	{"",        "~/scripts/dwmblocks/sb-bluetooth",      5,                  4},
+	{"",        "~/scripts/dwmblocks/sb-wlan",	     5,		         5}, 
+	{"",        "~/scripts/dwmblocks/sb-clock",	     5,		         6},
+	{"",        "~/scripts/dwmblocks/sb-power",          5,                  7},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 10;
