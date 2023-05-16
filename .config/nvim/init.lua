36499df951657ab485041a77a965e350feb16c7d
MA - Arch <matthewarcherr@gmail.com>
2023-05-16 17:29:49 +0100

    init

---
+++ .config/nvim/init.lua
@@ -0,0 +1,93 @@
+-- **************** Options ***************
+
+-- disabling netrw ( vim's builtin file explorer)
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.o.number = true         -- side bar numbering
+vim.o.relativenumber = true -- relative line numbers
+
+-- default tab settings
+vim.bo.expandtab   = true
+vim.bo.shiftwidth  = 0
+vim.bo.softtabstop = 2
+-- file specific tab settings
+vim.cmd[[autocmd FileType .hs setlocal shiftwidth=2]]
+
+-- colourscheme
+require 'nordic' .setup { theme = 'onedark', }
+vim.cmd.colorscheme 'nordic'
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- explorer setup
+require("nvim-tree").setup()
+
+-- tabline setup
+require("bufferline").setup{}
+
+-- statusline setup
+require("lualine").setup()
+
+
+-- **************** Bindings ***************
+local keymap = vim.api.nvim_set_keymap
+local nrm    = { noremap = true }
+
+--     MODE, BIND,      COMMAND,                       OPTIONS
+
+keymap("n", "ZS",       ":w<cr>",                      	nrm)        -- ZS writes file
+keymap("n", "SV",       ":source ~/.config/nvim/init.lua<cr>", nrm) -- SV reloads config
+
+-- nvim tree binds
+keymap("n", "<S-Tab>",  ":NvimTreeFindFileToggle<cr>", 	nrm) -- Shift-tab toggle explorer
+
+-- tab binds
+keymap("n", "<C-n>",    ":tabnew<cr>",                 	nrm) -- Crtl-n opens new tab
+
+-- telescope binds
+keymap("n", "<S-F>",    ":cd /home/matt | :Telescope find_files<cr>", nrm) -- Shift-F to open telescope
+
+
+-- window keybinds
+keymap("n", "WN",       ":belowright new<cr>",          nrm) -- WN new window
+keymap("n", "WQ",       ":quit<cr>",                   	nrm) -- WQ quit window
+keymap("n", "WT",       ":belowright 17sp +terminal<cr>", nrm) -- WT terminal window
+keymap("n", "WS",       ":belowright sp<cr>",           nrm) -- WS split window
+
+
+-- **************** Plugins ***************
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+-- colour scheme
+  use 'AlexvZyl/nordic.nvim'
+
+-- tabline
+  use { 'akinsho/bufferline.nvim', tag = "v3.*", 
+  requires = 'nvim-tree/nvim-web-devicons'}
+
+-- statusline
+  use { 'nvim-lualine/lualine.nvim', 
+  requires = { 'kyazdan/nvim-web-devicons', opt = true }}
+
+-- Sidebar file explorer
+  use { 'nvim-tree/nvim-tree.lua', 
+  requires = 'nvim-tree/nvim-web-devicons' }
+
+-- nvim telescope
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', 
+  requires = {'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'sharkdp/fd' } }
+
+-- haskell
+use {'neovimhaskell/haskell-vim'}
+-- use {
+--   'mrcjkb/haskell-tools.nvim',
+--   requires = {
+--     'neovim/nvim-lspconfig',
+--     'nvim-lua/plenary.nvim',
+--   },
+-- }
+
+end)
---
+++ .config/zsh/.zshrc
@@ -0,0 +1,43 @@
+# Created by newuser for 5.9
+
+# ----- environment variables -----
+# prompt
+autoload -U colors && colors
+PROMPT='[%F{blue}%n@%m%f %B%~%b]$ '
+
+# terminal history
+HISTSIZE=99999999
+SAVEHIST=99999999
+HISTFILE="${XDG_CASHE_HOME:-$HOME/.cache}/zsh/history"
+
+# ----- options -----
+autoload -U compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)
+
+#vim keys in menu
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '?' backward-delete-char
+
+#aliases
+alias vim="nvim"
+alias p="sudo pacman"
+alias SS="sudo systemctl"
+alias ls="ls -ogh --color"
+alias r="lf"
+alias spyder="conda run spyder"
+alias gc="git clone --recursive"
+alias shut="sudo shutdown now"
+alias xbl="xbacklight"
+alias odm="onedrive --monitor"
+alias ods="onedrive --synchronize"
+alias hss="hugo server --noHTTPCache --disableFastRender --bind 192.168.6.220 --baseURL http://192.168.6.220 --port 1313"
+
+# ----- plugins -----
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
