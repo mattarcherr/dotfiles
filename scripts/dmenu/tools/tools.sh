dd10139775384c5e81e992ca8ca44d48f53efb20
matt <matthewarcherr@gmail.com>
2023-07-03 20:49:41 +0100

    added screenshot script with additional functionality

--- scripts/dmenu/tools/tools.sh
+++ scripts/dmenu/tools/tools.sh
@@ -5,7 +5,7 @@ choices="Wallpaper\nScreenshot\nShortcuts\nManuals"
 chosen=$(echo -e "$choices" | dmenu -fn "JetBrainsMono Nerd Font:size=12" -l 20 -p " ")
 
 case "$chosen" in
-        Screenshot) scrot '/home/matt/Pictures/screenshots/%Y-%m-%d.png' ;;
+        Screenshot) ~/scripts/dmenu/tools/screenshot ;;
         Wallpaper) ~/scripts/dmenu/tools/wallpaper ;;
         Shortcuts) ~/scripts/dmenu/tools/shortcuts ;;
         Manuals) ~/scripts/dmenu/tools/manuals
