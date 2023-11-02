410d78f0b45ce2ce7cb3cf8e3ab430d2c87d469e
matt <matthewarcherr@gmail.com>
2023-11-02 16:01:07 +0000

    added passmenu to tools list

--- scripts/dmenu/tools/tools.sh
+++ scripts/dmenu/tools/tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choices="Wallpaper\nScreenshot\nShortcuts\nManuals"
+choices="Wallpaper\nScreenshot\nShortcuts\nManuals\nPasswords"
 
 chosen=$(echo -e "$choices" | dmenu -fn "JetBrainsMono Nerd Font:size=12" -l 20 -p " ")
 
@@ -8,5 +8,6 @@ case "$chosen" in
         Screenshot) ~/scripts/dmenu/tools/screenshot ;;
         Wallpaper) ~/scripts/dmenu/tools/wallpaper ;;
         Shortcuts) ~/scripts/dmenu/tools/shortcuts ;;
-        Manuals) ~/scripts/dmenu/tools/manuals
+        Manuals) ~/scripts/dmenu/tools/manuals ;;
+        Passwords) ~/scripts/dmenu/tools/passwords
 esac
