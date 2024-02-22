66341988794eb97dc1fba322f396f01a3e65cb13
matt <matthewarcherr@gmail.com>
2024-02-22 19:28:46 +0000

    Added more dwm tags, changed log out command in shutdown script

--- SL/dwm/config.def.h
+++ SL/dwm/config.def.h
@@ -55,7 +55,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* ******************** Tags/Workspaces ******************** */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
